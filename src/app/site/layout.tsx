5db81ee3ef61856a616dc036a60a20d79ea8c8aa
YuPain <huyup1e2n3g@163.com>
2024-02-04 21:08:50 +0800

    update app/site/layout.tsx

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -3,11 +3,7 @@ import { dark } from "@clerk/themes";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
-      <main className="h-full">{children}</main>
-    </ClerkProvider>
-  );
+  return <main className="h-full">{children}</main>;
 };
 
 export default layout;
